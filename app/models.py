7b7c60c1d7705193474372379f1b4e33e9acbc80
Gracesvg <gracemzerah71@gmail.com>
2023-07-17 13:49:21 +0300

    added file

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+
 # Create your models here.
+
+class People(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False)
+    school = models.CharField(max_length=20, blank=False, null=False)
+    email = models.EmailField()
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import People
 
 
 # Create your views here.
@@ -7,3 +8,21 @@ from django.http import HttpResponse
 
 def home(request):
     return render(request, 'index.html')
+
+
+def insert(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        school = request.POST.get('school')
+        email = request.POST.get('email')
+
+        person = People(name=name, email=email, school=school)
+        person.save()
+
+        # print(name, school)
+        return HttpResponse("success")
+
+def people(request):
+    d=People.objects.all()
+    context = {"data": d}
+    return render(request,'People.html',context)
